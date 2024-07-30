6cbb7bac6552d5b48cb2d6eb48bf0f0ab8f38699
PrabhneetSingh02 <prabhneet2002@gmail.com>
2024-07-30 10:54:35 +0530

    new clid

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ provider "azurerm" {
   features {}
   subscription_id   = "a5e05dea-afeb-490f-bdbc-88b3a91dc61c"
   tenant_id         = "e77593b4-fe05-4586-8fce-2e424399caaf"
-  client_id         = "f941eee9-f905-4080-bf3f-8315b3be3c62"
-  client_secret     = "CxQ8Q~MpBUQ1IHS8tNuVKKsW5-MpmBm2eos6EbpD"
+  client_id         = "4c018b7b-ad0e-4db4-821d-cad634eab07d"
+  client_secret     = "RxL8Q~3Sqj4K0WttmBMB8.SkRF6VLfbEBfybSdd3"
 }
